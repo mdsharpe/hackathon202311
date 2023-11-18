cc23bffb2862c058e2855941815415523f17a3a9
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-11-18 11:28:54 +0000

    Fixing

--- src/Shared/IGameHub.cs
+++ src/Shared/IGameHub.cs
@@ -5,5 +5,5 @@ namespace Shared;
 public interface IGameHub
 {
     Task<GameBoard> GetBoard();
-    Task MoveTile(int x, int y, Direction direction);
+    Task MoveTile(Coordinates sourceCoordinates, Direction direction);
 }
