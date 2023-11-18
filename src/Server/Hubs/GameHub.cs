152df2f9ca2b00b94ed9f8f6364eabdc33a007e2
Dylan Whitaker <dylan.whitaker@enable.com>
2023-11-18 11:27:00 +0000

    Add move tile validation

--- src/Server/Hubs/GameHub.cs
+++ src/Server/Hubs/GameHub.cs
@@ -7,18 +7,53 @@ namespace Server.Hubs;
 public class GameHub : Hub<IGameHub>
 {
     private readonly GameBoard _gameBoard;
+    private readonly ILogger<GameHub> _logger;
 
     public GameHub(
         ILogger<GameHub> logger,
         GameBoard gameBoard)
     {
         _gameBoard = gameBoard;
+        _logger = logger;
     }
 
     public async Task MoveTile(int x, int y, Direction direction)
     {
+        try
+        {
+            DiscardInvalidMoves(x, y, direction);
+        }
+        catch (InvalidOperationException ex)
+        {
+            _logger.LogInformation(ex.Message);
+        }
+
         // TODO move tile in game board
 
         await Clients.All.OnBoardChanged(_gameBoard);
+    }
+
+    private void DiscardInvalidMoves(int x, int y, Direction direction)
+    {
+        if (x == 0 && direction == Direction.Left)
+        {
+            throw new InvalidOperationException($"Can't move {nameof(direction)}");
+        }
+
+        if (x == _gameBoard.Tiles.First().Count && direction == Direction.Right)
+        {
+            // Assume all rows are the same length
+            throw new InvalidOperationException($"Can't move {nameof(direction)}");
+        }
+
+        if (y == 0 && direction == Direction.Down)
+        {
+            throw new InvalidOperationException($"Can't move {nameof(direction)}");
+        }
+
+        if (y == _gameBoard.Tiles.Count && direction == Direction.Up)
+        {
+            throw new InvalidOperationException($"Can't move {nameof(direction)}");
+        }
     }
 }
