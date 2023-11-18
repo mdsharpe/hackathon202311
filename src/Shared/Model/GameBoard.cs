adcb4f15ad3ddf7d6b8863bb9532153b5cda6fbe
Wen Gu <wen.gu@enable.com>
2023-11-18 11:55:50 +0000

    Solve conflicts

--- src/Shared/Model/GameBoard.cs
+++ src/Shared/Model/GameBoard.cs
@@ -1,6 +1,35 @@
-﻿namespace Shared.Model;
-
-public class GameBoard
+using System;
+namespace Shared.Model
 {
-    public Tile[,] Tiles { get; set; } = new Tile[,] { { } };
+    public class GameBoard
+    {
+        public GameBoard(Tile[,] tiles)
+        {
+            Tiles = tiles;
+        }
+
+        Tile[,] Tiles { get; set; }
+        private Array TileTypes { get; set; }
+
+        public void InitializeTiles(int xSize, int ySize)
+        {
+            TileTypes = Enum.GetValues(typeof(Tile));
+            for (int rowIndex = 0; rowIndex < ySize; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < xSize; columnIndex++)
+                {
+                    Tile randomTile = GenerateRandomTile();
+                    Tiles[rowIndex, columnIndex] = randomTile;
+                }
+            }
+        }
+
+        private Tile GenerateRandomTile()
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, TileTypes.Length);
+            Tile randomTile = (Tile)TileTypes.GetValue(randomIndex);
+            return randomTile;
+        }
+    }
 }
