c3e89665389d16a2a0ff162e6f254f676df9a598
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-11-18 10:00:10 +0000

    faedwa

---
+++ src/Server/Services/GameEngine.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using Shared.Model;
+
+namespace Server.Services;
+
+public class GameEngine : BackgroundService
+{
+    private static readonly TimeSpan Interval = TimeSpan.FromMilliseconds(1000);
+    private readonly GameState _gameState;
+
+    public GameEngine(GameState gameState)
+    {
+        _gameState = gameState;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var taskDelay = Task.Delay(Interval, stoppingToken);
+
+            // TODO here, do stuff we want to do the board e.g. create new tiles
+
+            await taskDelay;
+        }
+    }
+}
