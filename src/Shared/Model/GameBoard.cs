db9730d100c2953229c5b66a2dba783007ca087e
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-11-18 14:01:41 +0000

    Fixes

--- src/Shared/Model/GameBoard.cs
+++ src/Shared/Model/GameBoard.cs
@@ -1,32 +1,35 @@
 ﻿using System;
-namespace Shared.Model
-{
-    public class GameBoard
-    {
-        public Tile[][] Tiles { get; set; } = Array.Empty<Tile[]>();
-        private Array TileTypes { get; set; }
 
-        public void InitializeTiles(int xSize, int ySize)
+namespace Shared.Model;
+
+public class GameBoard
+{
+    public Tile[][] Tiles { get; set; } = Array.Empty<Tile[]>();
+    private Array TileTypes { get; set; }
+
+    public int Height => Tiles.Select(o => o.Length).DefaultIfEmpty().Max();
+    public int Width => Tiles.Length;
+
+    public void InitializeTiles(int xSize, int ySize)
+    {
+        TileTypes = Enum.GetValues(typeof(Tile));
+        Tiles = new Tile[ySize][];
+        for (int rowIndex = 0; rowIndex < ySize; rowIndex++)
         {
-            TileTypes = Enum.GetValues(typeof(Tile));
-            Tiles = new Tile[ySize][];
-            for (int rowIndex = 0; rowIndex < ySize; rowIndex++)
+            Tiles[rowIndex] = new Tile[xSize];
+            for (int columnIndex = 0; columnIndex < xSize; columnIndex++)
             {
-                Tiles[rowIndex] = new Tile[xSize];
-                for (int columnIndex = 0; columnIndex < xSize; columnIndex++)
-                {
-                    Tile randomTile = GenerateRandomTile();
-                    Tiles[rowIndex][columnIndex] = randomTile;
-                }
+                Tile randomTile = GenerateRandomTile();
+                Tiles[rowIndex][columnIndex] = randomTile;
             }
         }
+    }
 
-        private Tile GenerateRandomTile()
-        {
-            Random rnd = new Random();
-            int randomIndex = rnd.Next(0, TileTypes.Length);
-            Tile randomTile = (Tile)TileTypes.GetValue(randomIndex);
-            return randomTile;
-        }
+    private Tile GenerateRandomTile()
+    {
+        Random rnd = new Random();
+        int randomIndex = rnd.Next(0, TileTypes.Length);
+        Tile randomTile = (Tile)TileTypes.GetValue(randomIndex);
+        return randomTile;
     }
 }
