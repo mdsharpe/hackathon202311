bbb369ba16da70db58dd102e18f7cebe5d7c4428
Dylan Whitaker <dylan.whitaker@enable.com>
2023-11-18 12:02:06 +0000

    Add move tiles logic

--- src/Server/Hubs/GameHub.cs
+++ src/Server/Hubs/GameHub.cs
@@ -17,46 +17,78 @@ public class GameHub : Hub
         _logger = logger;
     }
 
-    public async Task MoveTile(int x, int y, Direction direction)
+    public async Task MoveTile(Coordinates sourceCoordinates, Direction direction)
     {
         try
         {
-            DiscardInvalidMoves(x, y, direction);
+            DiscardInvalidMoves(sourceCoordinates, direction);
         }
         catch (InvalidOperationException ex)
         {
             _logger.LogInformation(ex.Message);
+            return;
         }
 
-        // TODO move tile in game board
+        // Get relevant tile data
+        var sourceTile = _gameBoard.Tiles[sourceCoordinates.X, sourceCoordinates.Y];
+        var targetCoordinates = GetTargetCoordinates(sourceCoordinates, direction);
+        var targetTile = _gameBoard.Tiles[targetCoordinates.X, targetCoordinates.Y];
+
+        // Swap tiles
+        _gameBoard.Tiles[sourceCoordinates.X,sourceCoordinates.Y] = targetTile;
+        _gameBoard.Tiles[targetCoordinates.X, targetCoordinates.Y] = sourceTile;
 
         await Clients.All.SendAsync(
             nameof(IGameHubClient.OnBoardChanged),
             _gameBoard);
     }
 
-    private void DiscardInvalidMoves(int x, int y, Direction direction)
+    private void DiscardInvalidMoves(Coordinates coordinates, Direction direction)
     {
-        if (x == 0 && direction == Direction.Left)
+        if (coordinates.X == 0 && direction == Direction.Left)
         {
             throw new InvalidOperationException($"Can't move {nameof(direction)}");
         }
 
-        if (x == _gameBoard.Tiles.GetLength(0) && direction == Direction.Right)
+        if (coordinates.X == _gameBoard.Tiles.GetLength(0) && direction == Direction.Right)
         {
             // Assume all rows are the same length
             throw new InvalidOperationException($"Can't move {nameof(direction)}");
         }
 
-        if (y == 0 && direction == Direction.Down)
+        if (coordinates.Y == 0 && direction == Direction.Down)
         {
             throw new InvalidOperationException($"Can't move {nameof(direction)}");
         }
 
-        if (y == _gameBoard.Tiles.GetLength(1) && direction == Direction.Up)
+        if (coordinates.Y == _gameBoard.Tiles.GetLength(1) && direction == Direction.Up)
         {
             // Assume all columns are the same length
             throw new InvalidOperationException($"Can't move {nameof(direction)}");
+        }
+    }
+
+    private Coordinates GetTargetCoordinates(Coordinates sourceCoordinates, Direction direction)
+    {
+        if (direction == Direction.Up)
+        {
+            return new Coordinates(sourceCoordinates.X, sourceCoordinates.Y + 1);
+        }
+        else if(direction == Direction.Down)
+        {
+            return new Coordinates(sourceCoordinates.X, sourceCoordinates.Y - 1);
+        }
+        else if (direction == Direction.Left)
+        {
+            return new Coordinates(sourceCoordinates.X - 1, sourceCoordinates.Y);
+        }
+        else if (direction == Direction.Right)
+        {
+            return new Coordinates(sourceCoordinates.X + 1, sourceCoordinates.Y);
+        }
+        else
+        {
+            throw new InvalidOperationException("How?");
         }
     }
 }
--- src/Shared/Model/Coordinates.cs
+++ src/Shared/Model/Coordinates.cs
@@ -1,6 +1,12 @@
 ﻿namespace Shared.Model;
 public class Coordinates
 {
+    public Coordinates(int x, int y)
+    {
+        X = x; 
+        Y = y;
+    }
+
     public int X = 0;
 
     public int Y = 0;
