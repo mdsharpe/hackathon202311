10ae718d63ba8b45f251b7d4c9f8ea6e1eb0eaa2
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-11-18 14:53:48 +0000

    stuff

---
+++ src/Client/TileCssUtil.cs
@@ -0,0 +1,7 @@
+﻿namespace Client;
+
+public static class TileCssUtil
+{
+    public static string Height = $"(100vh - 10rem) / {GlobalConstants.ySize}";
+    public static string Width = $"(100vw - 2rem - 80px) / {GlobalConstants.xSize}";
+}
