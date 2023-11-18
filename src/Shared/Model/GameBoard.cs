a4da51d13e5124568504b12d6091806eebbde5d0
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-11-18 12:07:48 +0000

    Fix

--- src/Shared/Model/GameBoard.cs
+++ src/Shared/Model/GameBoard.cs
@@ -14,8 +14,10 @@ namespace Shared.Model
         public void InitializeTiles(int xSize, int ySize)
         {
             TileTypes = Enum.GetValues(typeof(Tile));
+            Tiles = new Tile[ySize][];
             for (int rowIndex = 0; rowIndex < ySize; rowIndex++)
             {
+                Tiles[rowIndex] = new Tile[xSize];
                 for (int columnIndex = 0; columnIndex < xSize; columnIndex++)
                 {
                     Tile randomTile = GenerateRandomTile();
