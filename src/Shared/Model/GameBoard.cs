69038ba8207205220c8139e926007eda3f5274af
Matt Sharpe <3875370+mdsharpe@users.noreply.github.com>
2023-11-18 14:21:47 +0000

    wip

--- src/Shared/Model/GameBoard.cs
+++ src/Shared/Model/GameBoard.cs
@@ -7,22 +7,22 @@ public class GameBoard
     public Tile[][] Tiles { get; set; } = Array.Empty<Tile[]>();
     private Array TileTypes { get; set; }
 
-        public int Height => Tiles.Select(o => o.Length).DefaultIfEmpty().Max();
-        public int Width => Tiles.Length;
-        public void InitializeTiles(int xSize, int ySize)
+    public int Height => Tiles.Select(o => o.Length).DefaultIfEmpty().Max();
+    public int Width => Tiles.Length;
+    public void InitializeTiles(int xSize, int ySize)
+    {
+        TileTypes = Enum.GetValues(typeof(Tile)).OfType<Tile>().Where(tile => tile != Tile.EmptyCell).ToArray();
+        Tiles = new Tile[xSize][];
+        for (int rowIndex = 0; rowIndex < xSize; rowIndex++)
         {
-            TileTypes = Enum.GetValues(typeof(Tile)).OfType<Tile>().Where(tile => tile != Tile.EmptyCell).ToArray();
-            Tiles = new Tile[xSize][];
-            for (int rowIndex = 0; rowIndex < xSize; rowIndex++)
+            Tiles[rowIndex] = new Tile[ySize];
+            for (int columnIndex = 0; columnIndex < ySize; columnIndex++)
             {
-                Tiles[rowIndex] = new Tile[ySize];
-                for (int columnIndex = 0; columnIndex < ySize; columnIndex++)
-                {
-                    Tile randomTile = GenerateRandomTile();
-                    Tiles[rowIndex][columnIndex] = randomTile;
-                }
+                Tile randomTile = GenerateRandomTile();
+                Tiles[rowIndex][columnIndex] = randomTile;
             }
         }
+    }
 
     private Tile GenerateRandomTile()
     {
